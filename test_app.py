68ab2b90610dbaba4b8a2a321e5ac051bc394800
sanaaBens <bensmaa.sanaa@gmail.com>
2023-03-06 20:00:46 +0100

    f

--- test_app.py
+++ test_app.py
@@ -4,20 +4,20 @@ from unittest import TestCase
 
 
 class TestApp(TestCase):
-    def test_addition_true(self):
+    def test_sum_true(self):
         self.assertEqual(addition(1, 2), 3)
 
-    def test_addition_wrong(self):
+    def test_sum_wrong(self):
         self.assertNotEqual(addition(1, 2), 4)
 
-    def test_soustraction_true(self):
+    def test_minus_true(self):
         self.assertEqual(soustraction(2, 1), 1)
 
-    def test_soustraction_wrong(self):
+    def test_minus_wrong(self):
         self.assertNotEqual(soustraction(2, 1), 2)
 
-    def test_multiplication_true(self):
+    def test_multiply_true(self):
         self.assertEqual(multiplication(2, 2), 4)
 
-    def test_multiplication_wrong(self):
+    def test_multiply_wrong(self):
         self.assertNotEqual(multiplication(2, 2), 5)
